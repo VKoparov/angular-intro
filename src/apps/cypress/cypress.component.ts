f2fc07c7c7d7b7abdae1f1ee61ccd199ef933909
VKoparov <valentinkoparov@gmail.com>
2023-06-26 22:29:37 +0300

    feat(cypress): defined cypress home

--- src/apps/cypress/cypress.component.ts
+++ src/apps/cypress/cypress.component.ts
@@ -1,16 +1,43 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {Theme, THEME_CONFIG} from "../app/models/theme";
 
 @Component({
     selector: 'app-cypress',
     templateUrl: './cypress.component.html',
-    styleUrls: ['./cypress.component.css'],
-    encapsulation: ViewEncapsulation.None
+    styleUrls: ['./cypress.component.css']
 })
 export class CypressComponent {
 
+    protected theme!: Theme;
+
     header: string = 'Cypress';
 
-    links: { name: string, path: string }[] = [
-        { name: 'Component Inheritance', path: '/component-inheritance' }
+    groups: { name: string, items: { name: string, path: string }[] }[] = [
+        {
+            name: 'Alice', items: [
+                { name: 'Type of Test', path: '/component-inheritance' },
+                { name: 'Type of Test', path: '/component-inheritance' },
+                { name: 'Type of Test', path: '/component-inheritance' },
+                { name: 'Type of Test', path: '/component-inheritance' },
+                { name: 'Type of Test', path: '/component-inheritance' },
+                { name: 'Type of Test', path: '/component-inheritance' },
+                { name: 'Type of Test', path: '/component-inheritance' },
+                { name: 'Type of Test', path: '/component-inheritance' }
+            ]
+        },
+        {
+            name: 'Bob', items: [
+                { name: 'Type of Test', path: '/component-inheritance' },
+                { name: 'Type of Test', path: '/component-inheritance' },
+                { name: 'Type of Test', path: '/component-inheritance' },
+                { name: 'Type of Test', path: '/component-inheritance' }
+            ]
+        }
     ];
+
+    constructor(
+        @Inject(THEME_CONFIG) theme: Theme
+    ) {
+        this.theme = theme;
+    }
 }
